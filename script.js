42e42a05a40571f5de2eefc06f59e743d51908cb
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-13 14:58:58 +0100

    inserimento primo membro nell'array

---
+++ script.js
@@ -0,0 +1,11 @@
+// creazione array con le informazioni fornite
+
+const teamMembers = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "img/wayne-barnett-founder-ceo.jpg"
+    }
+]
+
+console.log(teamMembers);
